c49ae604f09261b6ba3ee1e1845417c5af01367d
Littlefisher <i@littlefisher.me>
2025-11-16 21:12:46 -0800

    bufebu

---
+++ examples/Linux/1_transparent_sched/app.cudafe1.c
@@ -0,0 +1,14 @@
+# 1 "app.cu"
+# 13 "app.cu"
+float *h_A = 0;
+# 13 "app.cu"
+float *h_B = 0;
+# 13 "app.cu"
+float *h_C = 0;
+float *d_A = 0;
+# 14 "app.cu"
+float *d_B = 0;
+# 14 "app.cu"
+float *d_C = 0;
+
+struct CUstream_st *stream = 0;
